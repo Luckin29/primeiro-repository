832e0ca01b8bed780abbe8bb21299a1e138bedcc
Lucas <lucas.lopes.moura@outlook.com>
2021-08-28 15:31:05 -0300

    Exercicios feitos até o 5(bonus)

---
+++ fundamentos-desenvolvimento-web/bloco-04-introducao-a-js-e-logica-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio-02.js
@@ -0,0 +1,6 @@
+
+let n = 5;
+asterisco = "*"
+    for(let i=0 ; i<=n; i+=1){
+        console.log(asterisco.repeat(i));
+}
---
+++ fundamentos-desenvolvimento-web/bloco-04-introducao-a-js-e-logica-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio-03.js
@@ -0,0 +1,8 @@
+
+let n = 7;
+asterisco = "*"
+espaco=" "
+    for(let i=0 ; i<=n; i+=1){
+
+        console.log((espaco.repeat(n-i)) + (asterisco.repeat(i)));
+}
