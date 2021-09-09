28c96659afcd69a70f6b7ddbe907b23b9d050764
Lucas <lucas.lopes.moura@outlook.com>
2021-09-08 23:03:02 -0300

    Segundo exercício completo

--- fundamentos-desenvolvimento-web/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
+++ fundamentos-desenvolvimento-web/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -19,15 +19,16 @@ function createDaysOfMonth() {
   const dezFriday = [4, 11, 18, 25];
   const dezHoliday = [24, 25, 31];
   const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
-  const recupera = document.getElementById('days');
+  const daysList = document.getElementById('days');
 
   for (let i = 0; i < dezDaysList.length; i += 1) {
     const days = dezDaysList[i];
     const dayListItem = document.createElement('li');
     dayListItem.innerHTML = days;
     dayListItem.className = 'day'
-    recupera.appendChild(dayListItem)
+    daysList.appendChild(dayListItem)
 
+ // Função para verificar se é feriado e adicionar a class 'holiday'
     function verifyHoliday() {
       for (let i = 0; i < dezHoliday.length; i += 1) {
         if(days === dezHoliday[i]) {
@@ -36,6 +37,7 @@ function createDaysOfMonth() {
       }
     }
 
+ // Função para verificar se é sexta-feira e adicionar a class 'friday'
     function verifyFriday() {
       for (let i = 0; i < dezFriday.length; i += 1) {
         if(days === dezFriday[i]) {
@@ -48,5 +50,14 @@ function createDaysOfMonth() {
   }
 
 }
-
 createDaysOfMonth();
+
+
+function createHolidayButton (feriados) {
+  const atual = document.getElementsByClassName('buttons-container')[0];
+  const botao = document.createElement('button');
+  botao.innerText = feriados
+  botao.id ='btn-holiday'
+  atual.appendChild(botao);
+}
+ createHolidayButton('Testando o botão');
